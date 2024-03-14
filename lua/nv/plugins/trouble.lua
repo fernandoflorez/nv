712befaae762220a472289f6496ced4eabf44b2a
Fernando Flórez <fernando@funciton.com>
2024-03-13 20:39:22 -0500

    added keymap to navigate between items in trouble

--- lua/nv/plugins/trouble.lua
+++ lua/nv/plugins/trouble.lua
@@ -3,7 +3,9 @@ return {
     event = { "BufReadPre", "BufNewFile" },
     dependencies = { "nvim-tree/nvim-web-devicons" },
     keys = {
-        { "<leader>xx", function() require("trouble").toggle() end, mode = "n" },
+        { "<leader>xx", function() require("trouble").toggle() end,                                    mode = "n" },
+        { "<leader>xn", function() require("trouble").next({ skip_groups = true, jump = true }) end,   mode = "n" },
+        { "<leader>xp", function() require("trouble").previous({ skip_groups = true, jump = true }) end, mode = "n" },
     },
     opts = {}
 }
