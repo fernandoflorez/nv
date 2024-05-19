1ce61f2a800dde98ae6d305198833155d3899925
Fernando Flórez <fernando@funciton.com>
2024-05-18 20:52:04 -0500

    added neotest for python

---
+++ lua/nv/plugins/neotest.lua
@@ -0,0 +1,27 @@
+return {
+    "nvim-neotest/neotest",
+    dependencies = {
+        "nvim-neotest/nvim-nio",
+        "nvim-lua/plenary.nvim",
+        "antoinemadec/FixCursorHold.nvim",
+        "nvim-treesitter/nvim-treesitter",
+        "nvim-neotest/neotest-python"
+    },
+    cmd = { "Neotest" },
+    keys = {
+        {
+            "<leader>tf",
+            function()
+                require("neotest").run.run(vim.fn.expand("%"))
+            end,
+            mode = "n"
+        }
+    },
+    config = function()
+        require("neotest").setup({
+            adapters = {
+                require("neotest-python")
+            }
+        })
+    end
+}
