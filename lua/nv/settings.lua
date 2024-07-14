185bc931090dda70b7ecbc6dc316c6609af116f2
Fernando Flórez <fernando@funciton.com>
2024-07-13 22:37:11 -0500

    added ignorecase to vim opts

--- lua/nv/settings.lua
+++ lua/nv/settings.lua
@@ -17,6 +17,7 @@ vim.opt.swapfile = false
 -- vim.opt.undodir = os.getenv("HOME") .. "/.vim/undodir"
 vim.opt.undofile = true
 vim.opt.incsearch = true
+vim.opt.ignorecase = true
 vim.opt.smartcase = true
 vim.opt.hlsearch = false
 vim.opt.termguicolors = true
