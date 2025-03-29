638e0b89ddb18f8e7d43b5a0abaf4d0aa23d0669
Fernando Flórez <fernando@funciton.com>
2025-03-28 22:32:59 -0500

    removed nerd font appearance

--- lua/nv/plugins/blink-cmp.lua
+++ lua/nv/plugins/blink-cmp.lua
@@ -39,10 +39,6 @@ return {
             ['<C-f>'] = { 'scroll_documentation_down', 'fallback' }
         },
 
-        appearance = {
-            nerd_font_variant = 'mono'
-        },
-
         completion = {
             documentation = { auto_show = true, auto_show_delay_ms = 0 },
             menu = {
