9cc7fb3f05c42c75875c828e9cd5dd99362f8417
Fernando Flórez <fernando@funciton.com>
2025-05-08 17:27:26 -0500

    mason lspconfig update

--- lua/nv/plugins/nvim-lspconfig.lua
+++ lua/nv/plugins/nvim-lspconfig.lua
@@ -16,6 +16,83 @@ return {
             vim.lsp.protocol.make_client_capabilities(),
             cmp_lsp.get_lsp_capabilities({}, false)
         )
+        vim.lsp.config('*', {
+            capabilities = capabilities
+        })
+        vim.lsp.config('ruff', {
+            capabilities = capabilities,
+            on_attach = function(client, _)
+                client.server_capabilities.hoverProvider = false
+            end
+        })
+        vim.lsp.config('basedpyright', {
+            capabilities = capabilities,
+            settings = {
+                basedpyright = {
+                    disableOrganizeImports = true,
+                    analysis = {
+                        ignore = { "*" },
+                        useLibraryCodeForTypes = true,
+                        typeCheckingMode = "standard",
+                        diagnosticMode = "openFilesOnly",
+                        autoImportCompletions = true,
+                    }
+                },
+            },
+        })
+        vim.lsp.config('lua_ls', {
+            capabilities = capabilities,
+            settings = {
+                Lua = {
+                    diagnostics = {
+                        globals = {
+                            "vim",
+                            "it",
+                            "describe",
+                            "before_each",
+                            "after_each"
+                        },
+                    },
+                }
+            }
+
+        })
+        vim.lsp.config('yamlls', {
+            capabilities = capabilities,
+            settings = {
+                yaml = {
+                    customTags = {
+                        "!Base64",
+                        "!Cidr",
+                        "!FindInMap sequence",
+                        "!GetAtt",
+                        "!GetAZs",
+                        "!ImportValue",
+                        "!Join sequence",
+                        "!Ref",
+                        "!Select sequence",
+                        "!Split sequence",
+                        "!Sub sequence",
+                        "!Sub",
+                        "!And sequence",
+                        "!Condition",
+                        "!Equals sequence",
+                        "!If sequence",
+                        "!Not sequence",
+                        "!Or sequence",
+                    },
+                }
+            }
+        })
+        vim.lsp.config('gopls', {
+            capabilities = capabilities,
+            settings = {
+                gopls = {
+                    gofumpt = true
+                }
+            }
+
+        })
         require("mason-lspconfig").setup({
             ensure_installed = {
                 "lua_ls",
@@ -26,95 +103,6 @@ return {
                 "graphql",
                 "gopls"
             },
-            handlers = {
-                function(server_name)
-                    require("lspconfig")[server_name].setup({
-                        capabilities = capabilities
-                    })
-                end,
-                ["ruff"] = function()
-                    require("lspconfig").ruff.setup({
-                        capabilities = capabilities,
-                        on_attach = function(client, _)
-                            client.server_capabilities.hoverProvider = false
-                        end
-                    })
-                end,
-                ["basedpyright"] = function()
-                    require("lspconfig").basedpyright.setup({
-                        capabilities = capabilities,
-                        settings = {
-                            basedpyright = {
-                                disableOrganizeImports = true,
-                                analysis = {
-                                    ignore = { "*" },
-                                    useLibraryCodeForTypes = true,
-                                    typeCheckingMode = "standard",
-                                    diagnosticMode = "openFilesOnly",
-                                    autoImportCompletions = true,
-                                }
-                            },
-                        },
-                    })
-                end,
-                ["lua_ls"] = function()
-                    require("lspconfig").lua_ls.setup({
-                        capabilities = capabilities,
-                        settings = {
-                            Lua = {
-                                diagnostics = {
-                                    globals = {
-                                        "vim",
-                                        "it",
-                                        "describe",
-                                        "before_each",
-                                        "after_each"
-                                    },
-                                },
-                            }
-                        }
-                    })
-                end,
-                ["yamlls"] = function()
-                    require("lspconfig").yamlls.setup({
-                        capabilities = capabilities,
-                        settings = {
-                            yaml = {
-                                customTags = {
-                                    "!Base64",
-                                    "!Cidr",
-                                    "!FindInMap sequence",
-                                    "!GetAtt",
-                                    "!GetAZs",
-                                    "!ImportValue",
-                                    "!Join sequence",
-                                    "!Ref",
-                                    "!Select sequence",
-                                    "!Split sequence",
-                                    "!Sub sequence",
-                                    "!Sub",
-                                    "!And sequence",
-                                    "!Condition",
-                                    "!Equals sequence",
-                                    "!If sequence",
-                                    "!Not sequence",
-                                    "!Or sequence",
-                                },
-                            }
-                        }
-                    })
-                end,
-                ["gopls"] = function()
-                    require("lspconfig").gopls.setup({
-                        capabilities = capabilities,
-                        settings = {
-                            gopls = {
-                                gofumpt = true
-                            }
-                        }
-                    })
-                end
-            }
         })
         require('mason-tool-installer').setup({
             ensure_installed = {
