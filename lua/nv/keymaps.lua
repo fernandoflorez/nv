7573c0613d4f8ff93e342fa3de51eacce8324a2e
Fernando Flórez <fernando@funciton.com>
2025-12-08 11:40:36 -0500

    updates to packages and moved things around a little

--- lua/nv/keymaps.lua
+++ lua/nv/keymaps.lua
@@ -2,6 +2,10 @@ vim.keymap.set("n", "<S-Up>", "<cmd>resize +1<cr>")
 vim.keymap.set("n", "<S-Down>", "<cmd>resize -1<cr>")
 vim.keymap.set("n", "<S-Left>", "<cmd>vertical resize -1<cr>")
 vim.keymap.set("n", "<S-Right>", "<cmd>vertical resize +1<cr>")
+vim.keymap.set("n", "x", '"_x')
+vim.keymap.set("n", "dd", '"_dd')
+vim.keymap.set("n", "p", '"+p')
+vim.keymap.set("n", "P", '"+P')
 
 local nvGroup = vim.api.nvim_create_augroup("nv", {})
 vim.api.nvim_create_autocmd("LspAttach", {
@@ -10,11 +14,5 @@ vim.api.nvim_create_autocmd("LspAttach", {
         local opts = { buffer = event.buf, silent = true }
 
         vim.keymap.set("n", "gd", vim.lsp.buf.definition, opts)
-        -- vim.keymap.set("n", "<leader>rn", vim.lsp.buf.rename, opts)
-        -- vim.keymap.set("n", "<leader>ca", vim.lsp.buf.code_action, opts)
-        vim.keymap.set("n", "x", '"_x')
-        vim.keymap.set("n", "dd", '"_dd')
-        vim.keymap.set("n", "p", '"+p')
-        vim.keymap.set("n", "P", '"+P')
     end
 })
